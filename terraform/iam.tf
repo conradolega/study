2f09c9445d0e5276dcb7776feaf1a10663831c9a
Conrad Olega <conradolega@gmail.com>
2022-02-08 16:36:29 +0800

    Create Kinesis Analytics application

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -54,3 +54,70 @@ resource "aws_iam_role_policy_attachment" "kinesis_firehose" {
   role       = aws_iam_role.kinesis_firehose.name
   policy_arn = aws_iam_policy.kinesis_firehose.arn
 }
+
+resource "aws_iam_role" "kinesis_analytics" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "kinesisanalytics.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+data "aws_iam_policy_document" "kinesis_analytics" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kinesis:DescribeStream",
+      "kinesis:GetShardIterator",
+      "kinesis:GetRecords",
+      "kinesis:ListShards",
+    ]
+
+    resources = [
+      aws_kinesis_stream.test.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "firehose:DescribeDeliveryStream",
+      "firehose:PutRecord",
+      "firehose:PutRecordBatch",
+    ]
+
+    resources = [
+      aws_kinesis_firehose_delivery_stream.test.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "kinesis_analytics" {
+  policy = data.aws_iam_policy_document.kinesis_analytics.json
+}
+
+resource "aws_iam_role_policy_attachment" "kinesis_analytics" {
+  role       = aws_iam_role.kinesis_analytics.name
+  policy_arn = aws_iam_policy.kinesis_analytics.arn
+}
--- terraform/kinesis.tf
+++ terraform/kinesis.tf
@@ -18,3 +18,9 @@ resource "aws_kinesis_firehose_delivery_stream" "test" {
     prefix     = "firehose/"
   }
 }
+
+resource "aws_kinesisanalyticsv2_application" "test" {
+  name = "test"
+  runtime_environment = "FLINK-1_13"
+  service_execution_role = aws_iam_role.kinesis_analytics.arn
+}
