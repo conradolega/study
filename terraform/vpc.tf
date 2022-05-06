676ada4a2ef3d4b517b645e6a28051bc06140e3b
Conrad Olega <conradolega@gmail.com>
2022-05-06 10:44:49 +0800

    Create two custom VPCs and peer them together

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,3 +5,17 @@ data "aws_vpc" "default" {
 data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
+
+resource "aws_vpc" "vpc1" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "vpc2" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "aws_vpc_peering_connection" "connection" {
+  peer_vpc_id = aws_vpc.vpc2.id
+  vpc_id      = aws_vpc.vpc1.id
+  auto_accept = true
+}
